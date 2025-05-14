4b99ce15e86f754a17ebf7fe2192d0b1e5823739
methan6427 <adamkh0698@gmail.com>
2025-05-14 23:00:14 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './import/index.css';
+import './index.css';
 import App from './huffman/App';
-import reportWebVitals from './import/reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
